b9b5c73ffec4fe63f4b1492fb7b3478027e4a4a7
yZhang-lululvxing <yzhang@lululvxing.com>
2018-07-20 15:35:31 -0400

    Add scene5, remove one time key press for interaction

--- tessellation_demo_3D/main.cpp
+++ tessellation_demo_3D/main.cpp
@@ -52,6 +52,7 @@ Scene* createScene1(Shader* shader);
 Scene* createScene2(Shader* shader);
 Scene* createScene3(Shader* shader);
 Scene* createScene4(Shader* shader);
+Scene* createScene5(Shader* shader);
 
 
 // ======================= main ===========================
@@ -82,6 +83,12 @@ int main () {
                         "shader3_tes.glsl",
                         "shader4_gs.glsl",
                         "shader4_fs.glsl");
+
+  Shader shader_tess_5("shader3_vs.glsl",
+                        "shader3_tcs.glsl",
+                        "shader3_tes.glsl",
+                        "shader4_gs.glsl",
+                        "shader4_fs.glsl");
   // create scenes
   currentSceneIndex = 0;
   // add first scene
@@ -89,11 +96,13 @@ int main () {
   Scene * scene2 = createScene2(&shader_tess_2);
   Scene * scene3 = createScene3(&shader_tess_3);
   Scene * scene4 = createScene4(&shader_tess_4);
+  Scene * scene5 = createScene5(&shader_tess_5);
 
   allScenes.push_back(scene1);
   allScenes.push_back(scene2);
   allScenes.push_back(scene3);
   allScenes.push_back(scene4);
+  allScenes.push_back(scene5);
 
 
   glEnable(GL_DEPTH_TEST);
@@ -193,186 +202,91 @@ static GLFWwindow* init ()
  * W/E/R: rotate x, y. z
  * A/Z: move obj in z axis and tess
  * M/N: prev/next scene
+ * X: for scene 4, change draw line or not
  */
 static void handleKeyboard (GLFWwindow* window)
 {
     Scene* currentScene = allScenes[currentSceneIndex];
     glfwPollEvents();
     // handle key controls for controlling tessellation factors
-    static bool o_was_down = false;
-    static bool k_was_down = false;
-    static bool p_was_down = false;
-    static bool l_was_down = false;
-
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_O)) {
-      if (!o_was_down) {
-        currentScene->sceneTessllate(1.0, 0);
-        cout << "inner tess. factor = " << currentScene->suv.tess_fac_inner << endl;
-        o_was_down = true;
-
-      }
-    } else {
-      o_was_down = false;
+      currentScene->sceneTessllate(1.0, 0);
+      cout << "inner tess. factor = " << currentScene->suv.tess_fac_inner << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_K)) {
-      if (!k_was_down) {
-        currentScene->sceneTessllate(-1.0, 0);
-        cout << "inner tess. factor = " << currentScene->suv.tess_fac_inner << endl;
-        k_was_down = true;
-      }
-    } else {
-      k_was_down = false;
+      currentScene->sceneTessllate(-1.0, 0);
+      cout << "inner tess. factor = " << currentScene->suv.tess_fac_inner << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_P)) {
-      if (!p_was_down) {
-        currentScene->sceneTessllate(0, 1.0);
-        cout << "outer tess. factor = " << currentScene->suv.tess_fac_outer << endl;
-        p_was_down = true;
-      }
-    } else {
-      p_was_down = false;
+      currentScene->sceneTessllate(0, 1.0);
+      cout << "outer tess. factor = " << currentScene->suv.tess_fac_outer << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_L)) {
-      if (!l_was_down) {
-        currentScene->sceneTessllate(0, -1.0);
-        cout << "outer tess. factor = " << currentScene->suv.tess_fac_outer << endl;
-        l_was_down = true;
-      }
-    } else {
-      l_was_down = false;
+      currentScene->sceneTessllate(0, -1.0);
+      cout << "outer tess. factor = " << currentScene->suv.tess_fac_outer << endl;
     }
 
 
     // move obj by key left/right, up/down
-    static bool up_was_down = false;
-    static bool down_was_down = false;
-    static bool left_was_down = false;
-    static bool right_was_down = false;
-
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_LEFT)) {
-      if (!left_was_down) {
-        left_was_down = true;
-        currentScene->sceneTranslate(-0.1, 0, 0);
-      }
-    } else {
-      left_was_down = false;
+      currentScene->sceneTranslate(-0.1, 0, 0);
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_RIGHT)) {
-      if (!right_was_down) {
-        right_was_down = true;
-        currentScene->sceneTranslate(0.1, 0, 0);
-      }
-    } else {
-      right_was_down = false;
+      currentScene->sceneTranslate(0.1, 0, 0);
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_UP)) {
-      if (!up_was_down) {
-        up_was_down = true;
-        currentScene->sceneTranslate(0, 0.1, 0);
-      }
-    } else {
-      up_was_down = false;
+      currentScene->sceneTranslate(0, 0.1, 0);
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_DOWN)) {
-      if (!down_was_down) {
-        down_was_down = true;
-        currentScene->sceneTranslate(0, -0.1, 0);
-      }
-    } else {
-      down_was_down = false;
+      currentScene->sceneTranslate(0, -0.1, 0);
     }
 
     // move obj in z directeion and control in/out tess factor by keyborad A/Z
-    static bool a_was_down = false;
-    static bool z_was_down = false;
-
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_A)) {
-      if (!a_was_down) {
-        a_was_down = true;
-        currentScene->sceneTranslate(0, 0, 0.1);
-        currentScene->sceneTessllate(1.0, 1.0);
-        cout << "inner tess = " << currentScene->suv.tess_fac_inner;
-        cout << ", outer tess = " << currentScene->suv.tess_fac_outer << endl;
-      }
-    } else {
-      a_was_down = false;
+      currentScene->sceneTranslate(0, 0, 0.1);
+      currentScene->sceneTessllate(1.0, 1.0);
+      cout << "inner tess = " << currentScene->suv.tess_fac_inner;
+      cout << ", outer tess = " << currentScene->suv.tess_fac_outer << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_Z)) {
-      if (!z_was_down) {
-        z_was_down = true;
-        currentScene->sceneTranslate(0, 0, -0.1);
-        currentScene->sceneTessllate(-1.0, -1.0);
-        cout << "inner tess = " << currentScene->suv.tess_fac_inner;
-        cout << ", outer tess = " << currentScene->suv.tess_fac_outer << endl;
-      }
-    } else {
-      z_was_down = false;
+      currentScene->sceneTranslate(0, 0, -0.1);
+      currentScene->sceneTessllate(-1.0, -1.0);
+      cout << "inner tess = " << currentScene->suv.tess_fac_inner;
+      cout << ", outer tess = " << currentScene->suv.tess_fac_outer << endl;
     }
 
     // use w, e, r to set rotateion for x, y, z axis
-    static bool w_was_down = false;
-    static bool e_was_down = false;
-    static bool r_was_down = false;
-
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_W)) {
-      if (!w_was_down) {
-        w_was_down = true;
-        currentScene->sceneRotate(25, 1, 0, 0);
-        cout << "rotate by x axis for 25 degree" << endl;
-      }
-    } else {
-      w_was_down = false;
+      currentScene->sceneRotate(25, 1, 0, 0);
+      cout << "rotate by x axis" << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_E)) {
-      if (!e_was_down) {
-        e_was_down = true;
-        currentScene->sceneRotate(25, 0, 1, 0);
-        cout << "rotate by y axis for 25 degree" << endl;
-      }
-    } else {
-      e_was_down = false;
+      currentScene->sceneRotate(25, 0, 1, 0);
+      cout << "rotate by y axis" << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_R)) {
-      if (!r_was_down) {
-        r_was_down = true;
-        currentScene->sceneRotate(25, 0, 0, 1);
-        cout << "rotate by z axis for 25 degree" << endl;
-      }
-    } else {
-      r_was_down = false;
+      currentScene->sceneRotate(25, 0, 0, 1);
+      cout << "rotate by z axis" << endl;
     }
 
     // use s, x to set scale for objects
-    static bool s_was_down = false;
-    static bool x_was_down = false;
-
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_S)) {
-      if (!s_was_down) {
-        s_was_down = true;
-        currentScene->sceneScale(1.1, 1.1, 1.1);
-        cout << "scale: larger the object" << endl;
-      }
-    } else {
-      s_was_down = false;
+      currentScene->sceneScale(1.01, 1.01, 1.01);
+      cout << "scale: enlarger the object" << endl;
     }
 
     if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_X)) {
-      if (!x_was_down) {
-        x_was_down = true;
-        currentScene->sceneScale(0.9, 0.9, 0.9);
-        cout << "scale: smaller the object" << endl;
-      }
-    } else {
-      x_was_down = false;
+      currentScene->sceneScale(0.99, 0.99, 0.99);
+      cout << "scale: reduce the object" << endl;
     }
 
 
@@ -385,7 +299,7 @@ static void handleKeyboard (GLFWwindow* window)
         n_was_down = true;
         if (currentSceneIndex == 0) currentSceneIndex = allScenes.size() - 1;
         else currentSceneIndex--;
-        cout << "current scene: " << currentSceneIndex << endl;
+        cout << "current scene: " << currentSceneIndex + 1 << endl;
       }
     } else {
       n_was_down = false;
@@ -396,7 +310,7 @@ static void handleKeyboard (GLFWwindow* window)
         m_was_down = true;
         if (currentSceneIndex == allScenes.size() - 1) currentSceneIndex = 0;
         else currentSceneIndex++;
-        cout << "current scene: " << currentSceneIndex << endl;
+        cout << "current scene: " << currentSceneIndex + 1 << endl;
       }
     } else {
       m_was_down = false;
@@ -527,11 +441,13 @@ Scene* createScene3(Shader * shader) {
 
 /*
 * create scene 4: contains 3 3D dode with tess & geo shaders
-* draw in surface
+* draw in surface, with line show tess
 */
 Scene* createScene4(Shader * shader) {
-  // create objects
+  // set shader draw with line
+  shader->setInt("draw_line", 0);
 
+  // create objects
   Dodecahedron* d1 = new Dodecahedron("Orange");
   d1->setSize(3, 3, 3);
   d1->setRotate(45, 1, 0, 0);
@@ -550,10 +466,56 @@ Scene* createScene4(Shader * shader) {
   d2->sendUniformToShader();
 
 
-  Dodecahedron* d3 = new Dodecahedron(1.000, 0.412, 0.706, 0.88);
+  Dodecahedron* d3 = new Dodecahedron(1.000, 0.412, 0.706, 1.0);
   d3->setSize(1.5, 1.5, 1.5);
   d3->setRotate(60, 0, 1, 0);
-  d3->setLocation(0, -2, 1);
+  d3->setLocation(0, -2, -1);
+  d3->setDrawingMode(0);  // 0: fill, 1:line(default)
+  d3->setShaderProgram(shader);
+  d3->sendUniformToShader();
+
+  // create Scene
+  Scene * currentScene = new Scene(window_width, window_height);
+  currentScene->addObject(d1);
+  currentScene->addObject(d2);
+  currentScene->addObject(d3);
+  currentScene->addShader(shader);
+  currentScene->sendAllUniformToShaders();
+
+  return currentScene;
+}
+
+/*
+* create scene 5: contains 3 3D dode with tess & geo shaders
+* draw in surface
+*/
+Scene* createScene5(Shader * shader) {
+  // set shader draw without line
+  shader->setInt("draw_line", 1);
+
+  // create objects
+  Cube* d1 = new Cube(0.832, 0.223, 0.456, 1.0);
+  d1->setSize(0.8, 1.6, 2.2);
+  d1->setRotate(50, 0, 0, 1);
+  d1->setLocation(-3, 0, 2);
+  d1->setDrawingMode(0);  // 0: fill, 1:line(default)
+  d1->setShaderProgram(shader);
+  d1->sendUniformToShader();
+
+
+  Pyramid* d2 = new Pyramid("DogerBlue");
+  d2->setSize(2, 2, 1);
+  d2->setRotate(45, 0, 1, 0);
+  d2->setLocation(3, 1.5, 0.5);
+  d2->setDrawingMode(0);  // 0: fill, 1:line(default)
+  d2->setShaderProgram(shader);
+  d2->sendUniformToShader();
+
+
+  Dodecahedron* d3 = new Dodecahedron(0.223, 0.861, 0.145, 1.0);
+  d3->setSize(2, 2, 2);
+  d3->setRotate(60, 0, 1, 0);
+  d3->setLocation(0, -2, 0);
   d3->setDrawingMode(0);  // 0: fill, 1:line(default)
   d3->setShaderProgram(shader);
   d3->sendUniformToShader();
--- tessellation_demo_3D/shader4_fs.glsl
+++ tessellation_demo_3D/shader4_fs.glsl
@@ -12,7 +12,7 @@ uniform vec3 light_position;
 uniform vec3 diffuse_material;
 uniform vec3 ambient_material;
 
-uniform vec3 view_position;
+uniform int draw_line;
 
 out vec4 fragment_colour;
 
@@ -25,22 +25,34 @@ float amplify (float d, float scale, float offset) {
 }
 
 void main () {
+
 	// ambient
-	float ambient_strength = 0.5f;
- 	vec3 ambient = ambient_strength * ambient_material * light_color;
+	float ambient_strength = 0.2f;
+	vec3 ambient = ambient_strength * light_color;
 
 	// diffuse
 	vec3 norm = normalize(g_face_normal);
-	vec3 light_dir = normalize(light_position - g_tri_distance);
-	float diff = max(dot(norm, light_dir), 0.0);
-	vec3 diffuse = diff * diffuse_material * light_color;
+	float diff = abs(dot(norm, light_position));
+	vec3 diffuse = diff * light_color;
 
-	vec3 final = ambient + diffuse;
+  // specular
+	vec3 H = normalize(light_position + vec3(0, 0, 1));
+	float spec = abs(dot(norm, H));
+	float shininess = 0.1f;
+	vec3 specular_material = vec3(0.2f, 0.2f, 0.2f);
+	vec3 specular = pow(spec, shininess) * specular_material;
 
-	float d1 = min(min(g_patch_distance.x, g_patch_distance.y), g_patch_distance.z);
-	float d2 = min(min(g_tri_distance.x, g_tri_distance.y), g_tri_distance.z);
-	final =  amplify (d1, 40, -0.4) * amplify (d2, 60, -0.6) * object_color;
+	vec3 final;
 
-	fragment_colour = vec4(final, 1.0);
+	if (draw_line == 1) {
+		float d1 = min(min(g_tri_distance.x, g_tri_distance.y), g_tri_distance.z);
+		float d2 = min(min(g_patch_distance.x, g_patch_distance.y), g_patch_distance.z);
+		d1 = 1 - amplify(d1, 50, -0.5);
+		d2 = amplify(d2, 50, -0.5);
+		final = d2 * object_color + d1 * d2 * object_color;
+	} else {
+		final = (ambient + diffuse + specular) * object_color;
+	}
+		fragment_colour = vec4(final, 1.0);
 
 }
